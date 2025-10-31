47ba089bcae4505ce852e6536795faa04c2d2b81
Gabriele Sales <gbrsales@gmail.com>
2025-10-31 22:15:10 +0100

    Free space on GHA runner

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,7 +2,7 @@
 py_env <- BasiliskEnvironment(
   envname = "py_env",
   pkgname = "BatChef",
-  packages = c("python=3.12",
+  packages = c("python=3.12.2",
                "scanpy=1.11.4",
                "scipy=1.16.2",
                "anndata=0.12.2",
