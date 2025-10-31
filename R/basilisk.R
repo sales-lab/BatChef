3f988c643c17bf64ea861a7b7cd641e32c96eedb
Gabriele Sales <gbrsales@gmail.com>
2025-10-31 18:39:38 +0100

    Caching of pyenv and virtualenv directories

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,12 +1,25 @@
 #' @importFrom basilisk BasiliskEnvironment
-py_env <- BasiliskEnvironment(envname = "py_env",
-                              pkgname = "BatChef",
-                              packages = c("python=3.12",
-                                           "scanpy=1.11.4",
-                                           "scipy=1.16.2",
-                                           "anndata=0.12.2",
-                                           "scanorama=1.7.4",
-                                           "leidenalg=0.10.2",
-                                           "python-igraph=0.11.9",
-                                           "bbknn=1.6.0",
-                                           "scvi-tools=1.4.0"))
+py_env <- BasiliskEnvironment(
+  envname = "py_env",
+  pkgname = "BatChef",
+  packages = c("python=3.12",
+               "scanpy=1.11.4",
+               "scipy=1.16.2",
+               "anndata=0.12.2",
+               "scanorama=1.7.4",
+               "leidenalg=0.10.2",
+               "python-igraph=0.11.9",
+               "bbknn=1.6.0",
+               "scvi-tools=1.4.0")
+)
+
+print_basilisk_cache_paths <- function() {
+  proc <- basiliskStart(py_env)
+  on.exit(basiliskStop(proc))
+
+  invisible(basiliskRun(proc, fun=function() {
+    cfg <- reticulate::py_config()
+    cat(sprintf("PYENV_CACHE_DIR=%s\n", cfg$base_exec_prefix))
+    cat(sprintf("VIRTUALENV_CACHE_DIR=%s\n", cfg$virtualenv))
+  }))
+}
